5af3428cadf315f2cc29b283fff1a78d42335db5
gerardo <gerardo.gonzalez13@utrgv.edu>
2024-07-03 16:44:03 -0500

    down for maintenance

--- src/components/Maintenance.js
+++ src/components/Maintenance.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import '../assets/styles/App.css';
-import '../assets/styles/Maintenece.css';
+import '../assets/styles/Maintenance.css';
 const Maintenance = () => {
     return (
-        <div className='maintenece-container'>
+        <div className='maintenance-container'>
             <h1>We&apos;ll be back soon!</h1>
             <p>Sorry for the inconvenience but we&apos;re performing some maintenance at the moment. We&apos;ll be back online shortly!</p>
         </div>
