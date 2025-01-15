ed7966f268eec04e8b630dcffae281c82a8b5513
geragzz13 <102830887+geragzz13@users.noreply.github.com>
2025-01-15 12:32:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import UnderMaintenance from './pages/UnderMaintenance'; // Import the maintenance page
+import UnderMaintenance from './components/UnderMaintenance'; // Import the maintenance page
 import Navbar from './components/Navbar'; // Navigation bar component
 import Footer from './components/Footer'; // Footer component
 import Home from './pages/Home';
