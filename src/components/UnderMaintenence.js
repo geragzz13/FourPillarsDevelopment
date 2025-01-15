017cd98089059c069c795b2ec3e09ae062273554
geragzz13 <102830887+geragzz13@users.noreply.github.com>
2025-01-15 12:25:28 -0600

    Create UnderMaintenence.js

---
+++ src/components/UnderMaintenence.js
@@ -0,0 +1,11 @@
+// UnderMaintenance.js
+import React from 'react';
+
+const UnderMaintenance = () => (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+        <h1>Site Under Maintenance</h1>
+        <p>We’re currently working on improvements. Please check back later!</p>
+    </div>
+);
+
+export default UnderMaintenance;
